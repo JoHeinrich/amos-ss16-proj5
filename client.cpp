90962e08e9e494b3662caf621a2619b130073975
mambochaief <musidenmusiden@aol.com>
2016-05-03 08:13:23 +0200

    Client

---
+++ client.cpp
@@ -0,0 +1,79 @@
+#include <string.h>
+#include <cstring>
+#include <unistd.h>
+#include <stdio.h>
+#include <netdb.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <iomanip>
+#include <strings.h>
+#include <stdlib.h>
+#include <string>
+#include <time.h>
+#include <vector>
+using namespace std;
+
+int main (int argc, char* argv[])
+{
+    int listenFd, portNo;
+    bool loop = false;
+    struct sockaddr_in svrAdd;
+    struct hostent *server;
+    
+    if(argc < 3)
+    {
+        cerr<<"Syntax : ./client <host name> <port>"<<endl;
+        return 0;
+    }
+    
+    portNo = atoi(argv[2]);
+    
+    if((portNo > 65535) || (portNo < 2000))
+    {
+        cerr<<"Please enter port number between 2000 - 65535"<<endl;
+        return 0;
+    }       
+    
+    //create client skt
+    listenFd = socket(AF_INET, SOCK_STREAM, 0);
+    
+    if(listenFd < 0)
+    {
+        cerr << "Cannot open socket" << endl;
+        return 0;
+    }
+    
+    server = gethostbyname(argv[1]);
+    
+    if(server == NULL)
+    {
+        cerr << "Host does not exist" << endl;
+        return 0;
+    }
+    
+    bzero((char *) &svrAdd, sizeof(svrAdd));
+    svrAdd.sin_family = AF_INET;
+    
+    bcopy((char *) server -> h_addr, (char *) &svrAdd.sin_addr.s_addr, server -> h_length);
+    
+    svrAdd.sin_port = htons(portNo);
+    
+    int checker = connect(listenFd,(struct sockaddr *) &svrAdd, sizeof(svrAdd));
+    
+    if (checker < 0)
+    {
+        cerr << "Cannot connect!" << endl;
+        return 0;
+    }
+    
+    cout << "Object detected" << endl;
+    cout << "Warn other cars" << endl;
+
+    write(listenFd, "Caution! Object detected", strlen("Caution! Object detected"));
+
+    cout << "Warning sent" << endl;
+}
