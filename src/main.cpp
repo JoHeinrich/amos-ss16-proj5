971a692ade59d6175f1b02e16b198d49d0520416
JoHeinrich <jo-heinrich@web.de>
2016-05-06 15:13:52 +0200

    Local integration of Protobuf
    Integrated all the protobuf files into the main src directory
    removed messaging directory and merged the readme.rm files
    Tested locally on Ubuntu

--- src/main.cpp
+++ src/main.cpp
@@ -6,17 +6,25 @@ int main(int argc, char** argv)
 {
 	if(argc <= 1)
 	{
-		std::cout<<"Arg0: Control Index \nArg1-n: Controlspefific Commands\n";
+		
+		std::cerr << "Usage:  " << argv[0] << " MAIN_INDEX \n 1: hdf5\n 2: protobuf" << std::endl;
 	}
 	else
 	{
-		int index = atoi(argv[0]);
+		int index = atoi(argv[1]);
+		for(int m = 1; m < argc-1;m++)
+		{
+			argv[m] = argv[m+1];
+		}
+		argc --;
 		switch(index)
 		{
 			case 0: 
 				return main0();
+			case 1:
+				return main1(argc,argv);
 			default:
-				std::cout<<"Max ControlIndex is 0\n";
+				std::cout<<"Max ControlIndex is 1\n";
 		}
 	}
 }
