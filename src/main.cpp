ac0ee3cdf5537dcab4c37220cc655d1077a625ce
mambochaief <musidenmusiden@aol.com>
2016-05-10 15:56:03 +0200

    Added Communication subdirectory and add main3 & main4

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,7 @@ int main(int argc, const char** argv)
 	if(argc <= 1)
 	{
 		
-        std::cerr << "Usage:  " << argv[0] << " MAIN_INDEX \n 0: hdf5\n 1: protobuf\n 2: human detector" << std::endl;
+        std::cerr << "Usage:  " << argv[0] << " MAIN_INDEX \n 0: hdf5\n 1: protobuf\n 2: human detector\n 3: server\ 4: client" << std::endl;
 	}
 	else
 	{
@@ -23,8 +23,12 @@ int main(int argc, const char** argv)
 				return main0();
 			case 1:
 				return main1(argc,argv);
-            case 2:
-                return main2(argc, argv);
+            		case 2:
+                		return main2(argc, argv);
+			case 3:
+				return main3(argc, argv);
+			case 4:
+				return main4(argc, argv);
 			default:
 				std::cout<<"Max ControlIndex is 1\n";
 		}
