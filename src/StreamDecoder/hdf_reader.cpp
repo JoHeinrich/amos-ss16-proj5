a02149c70364a56de750c99e9949f47d3e560d36
Richard Fuchs <fuchsrichard@gmail.com>
2016-06-09 18:09:25 +0200

    removed unnecessary implemented function GetAllProtobufIds -because the Ids are converted to an index starting at 0 - going to GetImageCount

--- src/StreamDecoder/hdf_reader.cpp
+++ src/StreamDecoder/hdf_reader.cpp
@@ -335,10 +335,6 @@ std::string HDFReader::GetProtobufFilesDescription(){
     
 }
 
-std::vector<int> HDFReader::GetAllProtobufIds(){
-    return protobuf_file_ids_;
-}
-
 /*std::vector<std::vector<int64_t> > GetZValues(){
  
     return z_values_;
