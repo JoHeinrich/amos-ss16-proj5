7bd382939562bc21107fe6caf30e331879e8f00d
Richard Fuchs <fuchsrichard@gmail.com>
2016-06-11 21:40:29 +0200

    added files mp4_frame_selector.cpp and mp4_frame_selector.h

---
+++ src/StreamDecoder/mp4_frame_selector.h
@@ -0,0 +1,30 @@
+//
+// Projectname: amos-ss16-proj5
+//
+// Created on 21.05.2016.
+// Copyright (c) 2016 de.fau.cs.osr.amos2016.gruppe5
+//
+// This file is part of the AMOS Project 2016 @ FAU
+// (Friedrich-Alexander University Erlangen-Nürnberg)
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public
+// License along with this program. If not, see
+// <http://www.gnu.org/licenses/>.
+//
+
+#ifndef __amos_ss16_proj5__mp4_frame_selector__
+#define __amos_ss16_proj5__mp4_frame_selector__
+
+#include <stdio.h>
+
+#endif /* defined(__amos_ss16_proj5__mp4_frame_selector__) */
