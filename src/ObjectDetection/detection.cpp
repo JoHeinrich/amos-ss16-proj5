a79e780ed284172c44f62f8d619939d66960a8c8
nilshae <nilshaeusler@gmail.com>
2016-06-12 17:14:13 +0200

    changed function names according to coding rules

--- src/ObjectDetection/detection.cpp
+++ src/ObjectDetection/detection.cpp
@@ -28,7 +28,20 @@
 using namespace std;
 using namespace cv;
 
-Mat Detection::resizeFrame(Mat *frame) {
+void Detection::ProcessFrame(Mat *frame) {
+
+  PeopleDetector peopleDetector;
+  VehicleDetector vehicleDetector;
+
+  Mat resizedFrame = ResizeFrame(frame);
+
+  std::vector<Rect> detectedPeople = peopleDetector.Detect(&resizedFrame);
+  std::vector<Rect> detectedVehicles = vehicleDetector.Detect(&resizedFrame);
+
+  DisplayDetectedObjects(detectedPeople, detectedVehicles, &resizedFrame);
+}
+
+Mat Detection::ResizeFrame(Mat *frame) {
   //resize the image to width of 400px to reduce detection time and improve detection accuracy
   //0.3125 is used because the test video is 1280 x 720, so the width resized images is 400px this has to be changed to our image size (best would be no hard coded scaling so other images sizes work too!)
 
@@ -37,7 +50,7 @@ Mat Detection::resizeFrame(Mat *frame) {
   return resizedFrame;
 }
 
-void Detection::displayDetectedObjects(std::vector<Rect> firstDetection, std::vector<Rect> secondDetection, Mat *frame) {
+void Detection::DisplayDetectedObjects(std::vector<Rect> firstDetection, std::vector<Rect> secondDetection, Mat *frame) {
   //add retangle for each Object in firstDetection
   for (int i=0; i<firstDetection.size(); i++){
       Rect r = firstDetection[i];
@@ -51,17 +64,4 @@ void Detection::displayDetectedObjects(std::vector<Rect> firstDetection, std::ve
   }
 
   imshow("Camera stream", *frame);
-}
-
-void Detection::processFrame(Mat *frame) {
-
-  PeopleDetector peopleDetector;
-  VehicleDetector vehicleDetector;
-
-  Mat resizedFrame = resizeFrame(frame);
-
-  std::vector<Rect> detectedPeople = peopleDetector.detect(&resizedFrame);
-  std::vector<Rect> detectedVehicles = vehicleDetector.detect(&resizedFrame);
-
-  displayDetectedObjects(detectedPeople, detectedVehicles, &resizedFrame);
 }
