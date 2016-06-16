565c38dfe97989592327b33f74f678c4b519e200
Richard Fuchs <fuchsrichard@gmail.com>
2016-06-16 10:46:47 +0200

    fixed copyright info in files to fit our coding-rules

--- src/StreamDecoder/hdf5_frame_selector.cpp
+++ src/StreamDecoder/hdf5_frame_selector.cpp
@@ -1,8 +1,6 @@
 //
-// frame_selector.cpp
 // Projectname: amos-ss16-proj5
 //
-// Created on 21.05.2016.
 // Copyright (c) 2016 de.fau.cs.osr.amos2016.gruppe5
 //
 // This file is part of the AMOS Project 2016 @ FAU
