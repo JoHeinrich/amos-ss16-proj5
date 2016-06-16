766b0761335780e953f47038b498558452b99521
Richard Fuchs <fuchsrichard@gmail.com>
2016-06-16 10:46:47 +0200

    fixed copyright info in files to fit our coding-rules

--- src/StreamDecoder/image_view.cpp
+++ src/StreamDecoder/image_view.cpp
@@ -1,8 +1,6 @@
 //
-// image_view.cpp
 // Projectname: amos-ss16-proj5
 //
-// Created on 28.05.2016.
 // Copyright (c) 2016 de.fau.cs.osr.amos2016.gruppe5
 //
 // This file is part of the AMOS Project 2016 @ FAU
