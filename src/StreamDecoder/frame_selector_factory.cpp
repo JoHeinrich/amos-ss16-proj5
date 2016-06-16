d64d739108761f3ebb361021cf6626f7f4f54068
Richard Fuchs <fuchsrichard@gmail.com>
2016-06-16 10:50:45 +0200

    added draft for class FrameSelectorFactory

---
+++ src/StreamDecoder/frame_selector_factory.cpp
@@ -0,0 +1,24 @@
+//
+// Projectname: amos-ss16-proj5
+//
+// Copyright (c) 2016 de.fau.cs.osr.amos2016.gruppe5
+//
+// This file is part of the AMOS Project 2016 @ FAU
+// (Friedrich-Alexander University Erlangen-Nürnberg)
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public
+// License along with this program. If not, see
+// <http://www.gnu.org/licenses/>.
+//
+
+#include "frame_selector_factory.h"
