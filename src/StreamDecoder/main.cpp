b2fa01bd183f1eb2d05b95132606f3963106595d
Richard Fuchs <fuchsrichard@gmail.com>
2016-06-18 14:51:02 +0200

    changes function: main calls AnalyseVideo instead of PlayVideo

--- src/StreamDecoder/main.cpp
+++ src/StreamDecoder/main.cpp
@@ -44,7 +44,7 @@ int main(int argc, const char* argv[]) {
     if(argc == 2){
 
         Controller controller;
-        controller.PlayAsVideo(argv[1]);
+        controller.AnalyseVideo(argv[1]);
 
 
     } else if(argc == 3){
