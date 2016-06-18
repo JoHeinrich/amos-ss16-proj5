ee9981a050de5e14089cb478cf355772fc111019
Richard Fuchs <fuchsrichard@gmail.com>
2016-06-18 10:48:05 +0200

    added virtual functions to abstract Image class

--- src/StreamDecoder/image.h
+++ src/StreamDecoder/image.h
@@ -26,4 +26,44 @@
 
 #include <stdio.h>
 
+#include <opencv2/opencv.hpp>
+
+class Image{
+    
+public:
+    
+    /**
+     * Gets the image width
+     *
+     * @return The image width
+     */
+    virtual int GetImageWidth()=0;
+    
+    /**
+     * Gets the image height
+     *
+     * @return The image height
+     */
+    virtual int GetImageHeight()=0;
+    
+    /**
+     * Gets the bgr image
+     *
+     * @return The bgr image as opencv::Mat object
+     */
+    virtual cv::Mat GetRGBImage()=0;
+    
+    /**
+     * Returns the image payload
+     *
+     * @return the image payload
+     */
+    virtual std::string GetImagePayload()=0;
+    
+    
+private:
+
+
+};
+
 #endif /* defined(__amos_ss16_proj5__image__) */
