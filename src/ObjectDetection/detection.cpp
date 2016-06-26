6673c8d3be17ae4b8664f92ef682774f0df9ef04
Richard Fuchs <fuchsrichard@gmail.com>
2016-06-26 11:50:57 +0200

    implemnted method for function adjustContrastAndBrightness in class Detection

--- src/ObjectDetection/detection.cpp
+++ src/ObjectDetection/detection.cpp
@@ -140,3 +140,16 @@ Mat Detection::ResizeFrame(Mat *frame) {
     resize(*frame, resized_frame, Size(0, 0), 0.3125, 0.3125, CV_INTER_AREA);
     return resized_frame;
 }
+
+cv::Mat adjustContrastAndBrightness(cv::Mat *frame,  double contrastValue, int brightnessValue){
+    Mat adjusted_image = Mat::zeros( frame->size(), frame->type() );
+        for( int x = 0; x < frame->rows; x++ ){
+            for( int y = 0; y < frame->cols; y++ ){
+                // c: use all colours of RGB / BGR
+                for( int c = 0; c < 3; c++ ){
+                    adjusted_image.at<Vec3b>(x,y)[c] = saturate_cast<uchar>(contrastValue* (frame->at<Vec3b>(x,y)[c])+ brightnessValue );
+            }
+        }
+    }
+    return adjusted_image;
+}
