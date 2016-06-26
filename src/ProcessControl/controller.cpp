0e1f15fbd9a7bede86f70781a9f4826516790c53
Richard Fuchs <fuchsrichard@gmail.com>
2016-06-26 12:25:39 +0200

    switched from HOGPeopleDetector to DaimlerPeopleDetector in Controller

--- src/ProcessControl/controller.cpp
+++ src/ProcessControl/controller.cpp
@@ -26,9 +26,9 @@
 
 //local
 #include "../ObjectDetection/cascade_haar_detector.h"
-// #include "../ObjectDetection/daimler_people_detector.h"
+#include "../ObjectDetection/daimler_people_detector.h"
 #include "../ObjectDetection/detection.h"
-#include "../ObjectDetection/hog_people_detector.h"
+//#include "../ObjectDetection/hog_people_detector.h"
 #include "../StreamDecoder/image_view.h"
 #include "../ScenarioAnalysation/scenario.h"
 #include "../ScenarioAnalysation/humans_in_front_of_bus_scenario.h"
@@ -60,8 +60,8 @@ void Controller::AnalyseVideo(std::string videofile, uint16_t port, std::string 
   FrameSelector* pipeline = frame_selector_factory.GetFrameSelector();
   int protobuf_counts = pipeline->GetImageCount();
 
-  // DaimlerPeopleDetector people_detector;
-  HOGPeopleDetector people_detector;
+  DaimlerPeopleDetector people_detector;
+  //HOGPeopleDetector people_detector;
   CascadeHaarDetector vehicle_detector("cars3.xml");
   Detection detection(&people_detector, &vehicle_detector);
 
