fb64bdd333fe2b23b023e0e7cfe19693d3c4c590
nilshae <nilshaeusler@gmail.com>
2016-07-05 14:31:04 +0200

    final fix for bounding box at wrong location bug

--- src/ProcessControl/controller.cpp
+++ src/ProcessControl/controller.cpp
@@ -64,11 +64,10 @@ void Controller::AnalyseVideo(std::string videofile) {
 
   //DaimlerPeopleDetector people_detector;
   //HOGPeopleDetector people_detector;
-  TemplateMatchingPeopleDetector people_detector("../../src/ObjectDetection/template.png");
-
+  // TemplateMatchingPeopleDetector people_detector("../../src/ObjectDetection/template.png");
 
   CascadeHaarDetector vehicle_detector("cars3.xml");
- // CascadeHaarDetector people_detector("haarcascade_fullbody.xml", 1.5, 0, cv::Size(14,28), cv::Size(56,112));
+  CascadeHaarDetector people_detector("haarcascade_fullbody.xml", 1.5, 0, cv::Size(14,28), cv::Size(56,112));
   Detection detection(&people_detector, &vehicle_detector);
 
   // set up all objects needed for analysing
@@ -77,7 +76,7 @@ void Controller::AnalyseVideo(std::string videofile) {
   Analyser analyser(possible_scenarios);
 
 
-  
+
 
   for (int i=0; i<protobuf_counts; i++) {
 
